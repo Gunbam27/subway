9f4c894806a4951caee61ba69b8fd8dda5ebd339
hee462 <a01080809887@gmail.com>
2024-04-15 18:06:35 +0900

    feat : 스플래쉬 구현

---
+++ lib/ui/splash_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+import 'home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToMainScreen();
+  }
+
+  _navigateToMainScreen() async {
+    await Future.delayed(Duration(seconds: 3));
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => HomeScreen()),
+          (Route<dynamic> route) => false, // 뒤로가기버튼 비활성
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Lottie.asset(
+          'assets/subway_2.json',
+          fit: BoxFit.fill,
+        ),
+      ),
+    );
+  }
+}

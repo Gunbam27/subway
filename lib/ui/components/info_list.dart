ce62b9b54ce8d4262644900f352432859c5e08bb
hee462 <a01080809887@gmail.com>
2024-04-15 18:06:23 +0900

    feat : 컴포넌츠 생성

--- lib/ui/components/info_list.dart
+++ lib/ui/components/info_list.dart
@@ -1,26 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:subway/data/model/subway_model.dart';
 
 class InfoList extends StatelessWidget {
-  const InfoList({super.key});
+ final SubwayModel subwayModel;
+  const InfoList({super.key, required this.subwayModel});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(10),
-      color: Colors.grey,
+      color: Color(0xff24c1ca),
       child: Column(
         children: [
-          Container(child: Text('지하철 역 이름')),
+          Container(
+            alignment: Alignment.center,
+              width: 200,
+              height: 50,
+              color: Color(0xff24c1ca),
+              child: Text(subwayModel.statnNm ,textAlign: TextAlign.center,style: TextStyle(fontSize: 16 , fontWeight: FontWeight.bold),)),
           Row(
             children: [
               Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration( color :Colors.cyan ,borderRadius: BorderRadius.circular(100)),
                   width: 100,
-                  height: 200,
-                  color: Colors.amber,
-                  child: Text('역 도착 및 운행')),
+                  height: 100,
+                  child: Text(subwayModel.trainLineNm)),
               Expanded(
                   child: Container(
-                      color: Colors.blue, height: 200, child: Text('메시지 표시'))),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(color: Colors.cyanAccent, borderRadius: BorderRadius.circular(15)),
+                    height: 200, child: Text("${subwayModel.arvlMsg2 } , ${subwayModel.arvlMsg3}"),
+                  )),
             ],
           ),
         ],

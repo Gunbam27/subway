af74425895a96fbbb8a7fcf2acc275ccf1bc655b
hee462 <a01080809887@gmail.com>
2024-04-15 19:32:42 +0900

    colorChange

--- lib/ui/components/info_list.dart
+++ lib/ui/components/info_list.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:subway/data/model/subway_model.dart';
 
 class InfoList extends StatelessWidget {
- final SubwayModel subwayModel;
+  final SubwayModel subwayModel;
+
   const InfoList({super.key, required this.subwayModel});
 
   @override
@@ -15,36 +16,69 @@ class InfoList extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
-              alignment: Alignment.center,
+                alignment: Alignment.center,
                 width: 200,
                 height: 50,
                 color: Color(0xff24c1ca),
-                child: Text(subwayModel.statnNm ,textAlign: TextAlign.center,style: TextStyle(fontSize: 16 , fontWeight: FontWeight.bold),)),
+                child: Text(
+                  subwayModel.trainLineNm,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                )),
           ),
           Row(
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Container(
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration( color :Colors.cyan ,borderRadius: BorderRadius.circular(100)),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: ColorChange(),
+                        borderRadius: BorderRadius.circular(100)),
                     width: 100,
                     height: 100,
-                    child: Text(subwayModel.trainLineNm , style: TextStyle(fontSize: 15 ,fontWeight: FontWeight.bold),)),
+                    child: Text(
+                      subwayModel.arvlMsg3,
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    )),
               ),
               Expanded(
                   child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(color: Colors.cyanAccent, borderRadius: BorderRadius.circular(15)),
-                      height: 200, child: Text("${subwayModel.arvlMsg2 } , ${subwayModel.arvlMsg3}"),
-                    ),
-                  )),
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.cyanAccent,
+                      borderRadius: BorderRadius.circular(15)),
+                  height: 200,
+                  child:
+                      Text("${subwayModel.arvlMsg2} , ${subwayModel.recptnDt}"),
+                ),
+              )),
             ],
           ),
         ],
       ),
     );
+  }
+
+Color ColorChange() {
+    switch (subwayModel.arvlCd) {
+      case 1:
+        return Colors.amberAccent;
+      case 2:
+        return Colors.grey;
+      case 3:
+        return Colors.red;
+      case 4:
+        return Colors.lightGreenAccent;
+      case 5:
+        return Colors.blue;
+      case 99:
+        return Colors.orange;
+      default:
+        return Colors.white;
+    }
   }
 }

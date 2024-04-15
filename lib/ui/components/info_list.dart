85e79979d9ad03963ec7a467ea030b99cda2ebaf
Yejin.Shin <mingmi_27@naver.com>
2024-04-15 19:36:19 +0900

    ui changed

--- lib/ui/components/info_list.dart
+++ lib/ui/components/info_list.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:subway/data/model/subway_model.dart';
 
 class InfoList extends StatelessWidget {
- final SubwayModel subwayModel;
+  final SubwayModel subwayModel;
   const InfoList({super.key, required this.subwayModel});
 
   @override
@@ -12,36 +12,47 @@ class InfoList extends StatelessWidget {
       color: Colors.white,
       child: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              alignment: Alignment.center,
-                width: 200,
-                height: 50,
-                color: Color(0xff24c1ca),
-                child: Text(subwayModel.statnNm ,textAlign: TextAlign.center,style: TextStyle(fontSize: 16 , fontWeight: FontWeight.bold),)),
+          Container(
+            alignment: Alignment.center,
+            height: 40,
+            color: Color(0xffF5F5F5),
+            child: Text(
+              subwayModel.trainLineNm,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
           ),
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration( color :Colors.cyan ,borderRadius: BorderRadius.circular(100)),
-                    width: 100,
-                    height: 100,
-                    child: Text(subwayModel.trainLineNm , style: TextStyle(fontSize: 15 ,fontWeight: FontWeight.bold),)),
-              ),
-              Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(color: Colors.cyanAccent, borderRadius: BorderRadius.circular(15)),
-                      height: 200, child: Text("${subwayModel.arvlMsg2 } , ${subwayModel.arvlMsg3}"),
+          Container(
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Colors.cyan,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
                     ),
-                  )),
-            ],
+                    width: 135,
+                    height: 40,
+                    child: Text(
+                      subwayModel.trainLineNm,
+                      style:
+                          TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Expanded(
+                  child:
+                      Text("${subwayModel.arvlMsg2} , ${subwayModel.arvlMsg3}"),
+                ),
+              ],
+            ),
           ),
         ],
       ),
